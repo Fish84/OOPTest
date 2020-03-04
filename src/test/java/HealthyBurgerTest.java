d40c2e60ba15a4634007b5004166866670fd0f28
Fish84 <craigacarr@gmail.com>
2020-03-04 15:10:57 +0200

    First Commit for Week 3 work

---
+++ src/test/java/HealthyBurgerTest.java
@@ -0,0 +1,48 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HealthyBurgerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void addHamburgerAddition1() {
+    }
+
+    @Test
+    public void addHamburgerAddition2() {
+    }
+
+    @Test
+    public void addHamburgerAddition3() {
+    }
+
+    @Test
+    public void addHamburgerAddition4() {
+    }
+
+    @Test
+    public void itemizeHamburger() {
+    }
+
+    @Test
+    public void addHealthAddition1() {
+    }
+
+    @Test
+    public void addHealthAddition2() {
+    }
+
+    @Test
+    public void testItemizeHamburger() {
+    }
+}
