65f2bed547c6ae084e16794901d69a6796dc78c4
Fish84 <craigacarr@gmail.com>
2020-03-04 16:16:59 +0200

    more tests

---
+++ src/test/java/HamburgerTest.java
@@ -0,0 +1,40 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HamburgerTest {
+    private Hamburger burger;
+
+    @Before
+    public void setUp() throws Exception {
+        burger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        System.out.println("Running a test...");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void addHamburgerAddition1() {
+
+    }
+
+    @Test
+    public void addHamburgerAddition2() {
+    }
+
+    @Test
+    public void addHamburgerAddition3() {
+    }
+
+    @Test
+    public void addHamburgerAddition4() {
+    }
+
+    @Test
+    public void itemizeHamburger() {
+    }
+}
